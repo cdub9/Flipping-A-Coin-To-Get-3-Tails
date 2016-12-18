9ba7dab4fcee385407ab7d44e1fd5c3600215c23
Coleman Warren <coleman.warren@hotmail.com>
2016-12-18 14:34:44 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<title>Flipping A Coin</title>
 	</head>
 	<body>
-	<p>How many times do you have to flip a coin to get three tails in a row?</p>
+	<h1>How many times do you have to flip a coin to get three tails in a row?</h1>
 	<?php
 	$tailsCount = 0;
 	$flipCount = 0;
@@ -22,7 +22,7 @@
 			echo "<div class=\"coin\">T</div>";
 		}
 	}
-	echo "<p>It took {$flipCount} flips to get three tails in a row!</p>";
+	echo "<h2>It took {$flipCount} flips to get three tails in a row!</h2>";
 	?>
 	</body>
 </html>
